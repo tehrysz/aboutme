7b33dd3dd2092b3aa36499247d44b4cc012c6618
Anna Julia C. Gonçalves <131499340+tehrysz@users.noreply.github.com>
2023-05-12 12:13:43 -0300

    Add files via upload

---
+++ card/card.js
@@ -0,0 +1,20 @@
+const blurOnHover = () => {
+    const element = document.getElementById('x');
+    element.classList.add("blur");
+    console.log("abacate")
+  }
+  
+  const removerblur = () => {
+    const element = document.getElementById('x');
+    element.classList.remove("blur");
+    console.log("neon")
+  }
+
+
+
+
+
+
+
+
+  
